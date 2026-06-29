b8db310efa90c710402c749c0b745de93817f711
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-06-29 22:47:01 +0800

    Update vite server config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,13 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -12,4 +19,3 @@ export default defineConfig({
     },
   },
 });
-
